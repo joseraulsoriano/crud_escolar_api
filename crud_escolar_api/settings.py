a06fa90510bda7543b1d64032c0a8a0b208ec173
joseraulsoriano <sc202357155@alm.buap.mx>
2025-05-10 16:35:33 -0600

    cambios

--- crud_escolar_api/settings.py
+++ crud_escolar_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-_&+lsebec(whhw!%n@ww&1j=4-^j_if9x8$q778+99oz&!ms2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
